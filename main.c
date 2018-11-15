650d6cf71661d52511e0e25c524df833ad5e0714
jangsooha <tngk2582@nate.com>
2018-11-15 16:26:00 +0900

    fifth submission

--- main.c
+++ main.c
@@ -3,52 +3,59 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int addfunc(int a, int b)
+void print_image(int image[5][5])
 {
-   return a+b;
+  int i, j;
+  int *ptr=&image[0][0];
+
+  for(i=0; i<5; i++)
+  {
+    for(j=0; j<5; j++)
+   {
+     printf("%d", *ptr);
+     ptr=ptr+1;
+    }
+  printf("\n");
+  }
 }
-int subfunc(int a, int b)
+
+
+void brighten_image(int image[5][5])
 {
-   return a-b;
+  int i, j;
+  int *ptr=&image[0][0];
+
+  for(i=0; i<5; i++)
+  {
+    for(j=0; j<5; j++)
+   {
+     *ptr=*ptr+10;
+     ptr=ptr+1;
+    }
+  
+  } 
 }
-int mulfunc(int a, int b)
-{
-   return a*b;
-}
-int divfunc(int a, int b)
-{
-   return a/b;
-}
+
 
 int main(int argc, char *argv[]) {
 
-  int i1, i2;
-  char m;
-  int (*f)(int, int);
+  int image[5][5]={
+    {10, 20, 30, 40, 50},
+    {10, 20, 30, 40, 50},
+    {10, 20, 30, 40, 50},
+    {10, 20, 30, 40, 50},
+    {10, 20, 30, 40, 50}
+};
 
-  printf("input calculation : ");
-  scanf("%d %c %d", &i1, &m, &i2);
-
-  switch(m)
-{
-   case '+':
-      f=addfunc;
-      break;
-   case '-':
-      f=subfunc;
-      break;
-   case '*':
-      f=mulfunc;
-      break;
-   case '/':
-      f=divfunc;
-      break;
+  print_image(image);
+  brighten_image(image);
+  print_image(image);
+	
+  return 0;
 }
+  
 
-   printf("result : %i", f(i1, i2));
 
-	return 0;
-}
 
 
 
